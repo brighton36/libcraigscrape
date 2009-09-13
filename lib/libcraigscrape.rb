d8058ac6dfd7616d495e3f6355880463eaa490dd
Chris DeRose <cderose@derosetechnologies.com>
2009-09-13 00:54:08 +0000

    - Added location_has, location_has_no to craigwatch - Fleshed out the CraigScrape: posts_since,posts, each_post, each_listing,listings methods and aded their rdoc

--- lib/libcraigscrape.rb
+++ lib/libcraigscrape.rb
@@ -3,24 +3,28 @@
 # All of libcraigscrape's objects and methods are loaded when you use <tt>require 'libcraigscrape'</tt> in your code.
 #
 
-require 'listings'
-require 'posting'
-require 'geo_listings'
-
 # A base class encapsulating the various libcraigscrape objects, and providing most of the 
 # craigslist interaction methods. Currently, we're supporting the old Class methods 
 # in a legacy-compatibility mode, but these methods are marked for deprecation. Instead,
 # create an instance of the Craigslist object, and use its Public Instance methods.
 # See the README for easy to follow examples.
+class CraigScrape; end
+
+require 'listings'
+require 'posting'
+require 'geo_listings'
+
 class CraigScrape
   cattr_accessor :time_now
+  cattr_accessor :site_to_url_prefix
   
   #--
   # NOTE:
-  # The only reason I took this out is b/c I might want to make it a cattr_accessor and test with a file:// 
-  # prefix.
+  # The only reason I took this out is b/c I might want to test with a file:// 
+  # prefix at some point
   #++
-  SITE_TO_URL_PREFIX = 'http://'
+  self.site_to_url_prefix = 'http://'
+
   
   # Takes a variable number of site/path specifiers (strings) as an argument. 
   # This list gets flattened and passed to CraigScrape::GeoListings.find_sites .
@@ -36,38 +40,70 @@ class CraigScrape
     @sites
   end
   
-  # TODO: what exactly is this rdoc going to be...? Maybe rename this to create_post_enumerator
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Passes the <b>first page listing</b> of each of these urls to the provided block.
   def each_listing(*fragments)
-    listing_urls_for(fragments).each do |url|
-      listing = Listings.new url
-      yield listing
+    listing_urls_for(fragments).each{|url| yield Listings.new(url) }
+  end
+  
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Passes <b>each page on every listing</b> for the passed URLs to the provided block.
+  def each_page_in_each_listing(*fragments)
+    each_listing(*fragments) do |listing|
+      listing.posts.each{|post| yield listing}
+      listing = listing.next_page 
+      retry if listing
     end
   end
   
-  # TODO: Rdoc? and test!
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Returns the <b>first page listing</b> of each of these urls to the provided block.
   def listings(*fragments)
-    catch :scrape_break do
-      listing_urls_for(fragments).collect do |url| 
-        listing = Listings.new url
-        throw :scrape_break if yield listing
-        listing
+    listing_urls_for(fragments).collect{|url| Listings.new url }
+  end
+  
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Passes all posts from each of these urls to the provided block, in the order they're parsed
+  # (for each listing, newest posts are returned first).
+  def each_post(*fragments)
+    each_page_in_each_listing(*fragments){ |l| l.posts.each{|p| yield p} }
+  end
+  
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Returns all posts from each of these urls, in the order they're parsed
+  # (newest posts first).
+  def posts(*fragments)
+    ret = []
+    each_page_in_each_listing(*fragments){ |l| ret += l.posts }
+    ret
+  end
+  
+  # Determines all listings which can be construed by combining the sites specified in the object
+  # constructor with the provided url-path fragments. 
+  #
+  # Returns all posts from each of these urls, which are newer than the provider 'newer_then' date.
+  # (Returns 'newest' posts first).
+  def posts_since(newer_then, *fragments)
+    ret = []
+    
+    fragments.each do |frag|
+      each_post(frag) do |p| 
+        break if p.post_date <= newer_then
+        ret << p
       end
     end
-  end
-  
-  # TODO: 
-  def each_post(*fragments)
-    
-  end
-  
-  # TODO: this actually replaces scrape_until
-  def posts(*fragments)
-    
-  end
-  
-  # TODO:   
-  def posts_since(listing, newer_then)
-    
+
+    ret    
   end
   
   class << self # Class methods
@@ -78,6 +114,8 @@ class CraigScrape
     #++
 
     # <b>This method is for legacy compatibility and is not recommended for use by new projects.</b>
+    # Instead, consider using CraigScrape::Listings.new 
+    #
     # Scrapes a single listing url and returns a Listings object representing the contents. 
     # Mostly here to preserve backwards-compatibility with the older api, CraigScrape::Listings.new "listing_url" does the same thing
     def scrape_listing(listing_url)    
@@ -85,6 +123,8 @@ class CraigScrape
     end
 
     # <b>This method is for legacy compatibility and is not recommended for use by new projects.</b>
+    # Instead, consider using the CraigScrape::each_post method.
+    #
     # Continually scrapes listings, using the supplied url as a starting point, until the supplied block returns true or
     # until there's no more 'next page' links available to click on
     def scrape_until(listing_url, &post_condition)
@@ -106,6 +146,8 @@ class CraigScrape
     end
 
     # <b>This method is for legacy compatibility and is not recommended for use by new projects.</b>
+    # Instead, consider using CraigScrape::Posting.new 
+    #
     # Scrapes a single Post Url, and returns a Posting object representing its contents.
     # Mostly here to preserve backwards-compatibility with the older api, CraigScrape::Listings.new "listing_url" does the same thing
     def scrape_full_post(post_url)
@@ -113,6 +155,8 @@ class CraigScrape
     end
 
     # <b>This method is for legacy compatibility and is not recommended for use by new projects.</b>
+    # Instead,  consider using the CraigScrape::each_post method.
+    #
     # Continually scrapes listings, using the supplied url as a starting point, until 'count' summaries have been retrieved
     # or no more 'next page' links are avialable to be clicked on. Returns an array of PostSummary objects.
     def scrape_posts(listing_url, count)
@@ -121,6 +165,8 @@ class CraigScrape
     end
 
     # <b>This method is for legacy compatibility and is not recommended for use by new projects.</b>
+    # Instead, consider using the CraigScrape::posts_since method.
+    #
     # Continually scrapes listings, until the date newer_then has been reached, or no more 'next page' links are avialable to be clicked on.
     # Returns an array of PostSummary objects. Dates are based on the Month/Day 'datestamps' reported in the listing summaries. 
     # As such, time-based cutoffs are not supported here. The scrape_until method, utilizing the SummaryPost.full_post method could achieve
@@ -134,13 +180,14 @@ class CraigScrape
   
   private
   
+  # This  takes a fragments paramter, and turns it into actual urls
   def listing_urls_for(listing_fragments)
     listing_fragments.collect{ |lf|
       # This removes any /'s from he beginning of the fragment
       lf = $1 if /^\/(.*)/.match lf
       # This adds a '/' to the end of a path, so long as its not a query we're dealing with...
       lf += '/' unless lf.index '?'
-      sites.collect { |site| '%s%s/%s' % [SITE_TO_URL_PREFIX,site,lf] }
+      sites.collect { |site| '%s%s/%s' % [site_to_url_prefix,site,lf] }
     }.flatten
   end
     
