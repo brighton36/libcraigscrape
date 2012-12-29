292039709570d3d17c75c723a42827f0b3970182
Chris DeRose <cderose@Chriss-MacBook-Air.local>
2012-12-29 18:40:16 -0500

    Fixe price parsing issues in the listing, adjusted craigwatch to use the money gem

--- spec/listings_spec.rb
+++ spec/listings_spec.rb
@@ -17,7 +17,7 @@ describe CraigScrape::Listings do
         :post_date => Date.parse('2012/11/26') }) }
     specify{ subject.posts[1].attributes.should eq({
         :label     => 'we buy junk-bus-truck- car for cash!!$500-$5000-5612062848',
-        :price     => 5000,
+        :price     => Money.new(500000, 'USD'),
         :href      => 'http://miami.craigslist.org/brw/ctd/3437083983.html',
         :url       => 'http://miami.craigslist.org/brw/ctd/3437083983.html',
         :location  => 'all over',
@@ -33,5 +33,22 @@ describe CraigScrape::Listings do
     specify{ subject.posts.collect(&:post_date).uniq.should eq(['12/12/2012', 
       '12/11/2012', '12/10/2012'].collect{|t| Date.strptime(t, "%m/%d/%Y") } ) }
     specify{ subject.next_page_href.should eq('http://newyork.craigslist.org/search/ppa?query=kenmore&srchType=A&s=100') }
+    specify{ subject.posts[0].attributes.should eq({
+        :label     => 'Staten island appliance repair',
+        :href      => 'http://newyork.craigslist.org/stn/app/3440211032.html',
+        :url       => 'http://newyork.craigslist.org/stn/app/3440211032.html',
+        :location  => '7184487435',
+        :section   => 'appliances - by owner',
+        :img_types => [],
+        :post_date => Date.parse('2012/12/12') }) }
+    specify{ subject.posts[1].attributes.should eq({
+        :label     => 'Kenmore 5200 BTU Air Conditioner',
+        :href      => 'http://newyork.craigslist.org/mnh/app/3474408782.html',
+        :url       => 'http://newyork.craigslist.org/mnh/app/3474408782.html',
+        :location  => 'Upper West Side',
+        :section   => 'appliances - by owner',
+        :img_types => [:pic],
+        :price     =>  Money.new(3000, 'USD'),
+        :post_date => Date.parse('2012/12/12') }) }
   end
 end
